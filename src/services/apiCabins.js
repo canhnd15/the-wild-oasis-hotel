1663b21320c6743a9abf4a7980c433e094af9808
canhnd <canhnd@Canhs-Laptop.local>
2024-03-09 23:55:22 +0700

    tuning code

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -30,42 +30,25 @@ export async function createUpdateCabin(newCabin, id) {
     ? newCabin.image
     : `${SUPABASE_URL}/storage/v1/object/public/cabins_images/${createdImageName}`;
 
+  let query = supabase.from("cabins");
+
   // 1.1 - Insert action
   if (!id) {
-    const { data, error } = await supabase
-      .from("cabins")
-      .insert([
-        {
-          name: newCabin.name,
-          max_capacity: newCabin.maxCapacity,
-          regular_price: newCabin.regularPrice,
-          discount: newCabin.discount,
-          description: newCabin.description,
-          image: imagePath,
-        },
-      ])
-      .select();
-
-    // 2. Upload image
-    const { error: storageError } = await supabase.storage
-      .from("cabins_images") //
-      .upload(createdImageName, newCabin.image);
-
-    // 3. Delete cabin if upload was error.
-    if (storageError) {
-      await supabase.from("cabins").delete().eq("id", data.id);
-      throw new Error("Could upload cabin image to storage.");
-    }
-
-    if (error)
-      throw new Error(`Cabin could not be inserted. [${error.message}]`);
-    return data;
+    query = query.insert([
+      {
+        name: newCabin.name,
+        max_capacity: newCabin.maxCapacity,
+        regular_price: newCabin.regularPrice,
+        discount: newCabin.discount,
+        description: newCabin.description,
+        image: imagePath,
+      },
+    ]);
   }
 
   // 1.2 - Update action
   if (id) {
-    const { data, error } = await supabase
-      .from("cabins")
+    query = query
       .update({
         name: newCabin.name,
         max_capacity: newCabin.maxCapacity,
@@ -74,22 +57,22 @@ export async function createUpdateCabin(newCabin, id) {
         description: newCabin.description,
         image: imagePath,
       })
-      .eq("id", id)
-      .select();
-
-    // 2. Upload image
-    const { error: storageError } = await supabase.storage
-      .from("cabins_images") //
-      .upload(createdImageName, newCabin.image);
-
-    // 3. Delete cabin if upload was error.
-    if (storageError) {
-      await supabase.from("cabins").delete().eq("id", data.id);
-      throw new Error("Could upload cabin image to storage.");
-    }
-
-    if (error)
-      throw new Error(`Cabin could not be updated. [${error.message}]`);
-    return data;
+      .eq("id", id);
   }
+
+  const { data, error } = await query.select().single();
+  if (error) throw new Error(`Cabin could not be inserted. [${error.message}]`);
+
+  //2. Upload image
+  const { error: storageError } = await supabase.storage
+    .from("cabins_images") //
+    .upload(createdImageName, newCabin.image);
+
+  // 3. Delete cabin if upload was error.
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    throw new Error("Could upload cabin image to storage.");
+  }
+
+  return data;
 }
